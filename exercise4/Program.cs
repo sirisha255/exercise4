74504ba8b5bb1eb1469fd7bb9019bf9064f41d48
Sirisha <Sirisha@DESKTOP-856P7A7>
2022-09-09 19:52:12 +1200

    Add project files.

---
+++ exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the speed Limit value");
+            int speedlimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the speed of the car");
+            int carspeed = Convert.ToInt32(Console.ReadLine());
+            if (carspeed > speedlimit)
+            {
+                int failurePoints = 0;
+                while (carspeed > speedlimit)
+                {
+                    carspeed -= 5;
+                    failurePoints++;
+                }
+
+                if (failurePoints > 12)
+                    Console.WriteLine("License Suspended.");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
+
+        }
+    }
+}
